4111bda45228374f6442f136b58a9ad0f4dadac3
zzangjyj0818 <jyj010818@naver.com>
2023-08-05 18:46:43 +0900

    Implement the Modal Architecture

--- front-end/src/components/Modal.jsx
+++ front-end/src/components/Modal.jsx
@@ -6,16 +6,37 @@ function Modal({closeModal, data}) {
   return (
     <div className="Modal">
       <div className="modalBody" onClick={(e) => e.stopPropagation()}>
-          <div>
-              <img src={data.url} alt='' className='img'/>
-              <div className='text'>
-                {data.name}
-                 <br/>
-                {data.price}원
+        <div className="Modal_Text">옵션을 선택하세요</div>
+        <div className="Modal_Container">
+          <img src={data.url} alt="" className="Modal_Img"/>
+          <div className="Modal_Wrapper">
+            <div>{data.name}</div>
+            <div className="Modal_Count">
+              <div>개수</div>
+              <div className="Modal_Count_Btn">
+                <div>-</div>
+                <div>{data.count}</div>
+                <div>+</div>
               </div>
-              <button onClick={closeModal}>취소하기</button>
-              <button>주문하기</button>
+            </div>
+            <div className="Modal_Ice_Btn">
+              <div>얼음량</div>
+                <div className="Modal_Ice">
+                  <div>적게</div>
+                  <div>기본</div>
+                  <div>많이</div>
+                </div>
+            </div>
           </div>
+        </div>
+        <hr/>
+        <div>
+          <div>가격 {data.price}</div>
+        </div>
+        <div>
+          <button onClick={closeModal}>취소</button>
+          <button>주문 담기</button>
+        </div>
       </div>
     </div>
   );
