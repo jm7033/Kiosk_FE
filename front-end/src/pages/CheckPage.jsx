07eef5b300dfe1994d3ac968ca166edbb0d06ddd
zzangjyj0818 <jyj010818@naver.com>
2023-08-07 04:40:00 +0900

    Implement the QR Reader ability

---
+++ front-end/src/pages/CheckPage.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// const CheckPage = ({receipt, setReceipt, cart, setCart}) => {
+//     return (
+//         <div>
+//             ㅎㅇ
+//         </div>
+//     );
+// };
+
+// export default CheckPage;
+
+
+export default Test;
